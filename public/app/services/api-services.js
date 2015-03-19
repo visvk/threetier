69f4221f2c1eb699f71411739e835d21788a7383
Visvk <viktor.sincak@gmail.com>
2015-03-19 14:46:32 +0100

    Angular: set API route to window.location

--- public/app/services/api-services.js
+++ public/app/services/api-services.js
@@ -1,8 +1,8 @@
 'use strict';
-console.log($window.location);
-var clusterUrl = $window.location.origin + '300/';
+console.log(window.location);
+var clusterUrl = window.location.origin + ':3000/';
 var noClusterUrl = 'http://localhost:3001/';
-var simpleUrl = $window.location.origin + '3002/';
+var simpleUrl = window.location.origin + ':3002/';
 var simpleWS = 'wss://localhost:3002/data';
 
 angular.module('myApp.apiServices', ['ngResource'])
