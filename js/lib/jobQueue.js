3be4b8b18015358296a81f72f02cf2e3a4df50dd
Visvk <viktor.sincak@gmail.com>
2015-04-02 17:47:43 +0200

    jobQueue.js added DOREDIS URL

--- js/lib/jobQueue.js
+++ js/lib/jobQueue.js
@@ -6,7 +6,12 @@
 
   logger = require('../lib/logger');
 
-  if (process.env.REDISTOGO_URL) {
+  if (process.env.DOREDIS_URL) {
+    redisOptions = {
+      port: 6379,
+      host: process.env.DOREDIS_URL
+    };
+  } else if (process.env.REDISTOGO_URL) {
     rtg = require("url").parse(process.env.REDISTOGO_URL);
     redisOptions = {
       port: rtg.port,
